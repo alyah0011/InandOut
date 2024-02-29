b42049c216d4ec51bb373b1fd00c81a117fa713b
Alya Husna Ibrahim <121216138+alyah0011@users.noreply.github.com>
2024-02-29 11:29:31 +0800

    Add files via upload

---
+++ Code/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Borrower { get; set; }
+		public string? Lender { get; set; }
+        [DisplayName("Item name")]//wont show wo space
+		public string ItemName { get; set; }
+
+	}
+}
